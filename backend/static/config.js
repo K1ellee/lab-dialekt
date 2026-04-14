a2c3db22fd171519b8fce0a5eff411f02cc77ca3
K1ellee <k1ellee@users.noreply.github.com>
2026-04-14 19:34:36 +0400

    Connect real Google Sheets data for Task 3

--- backend/static/config.js
+++ backend/static/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
-  // Если есть Google Sheets:
-  // 1) PUBLISHED CSV URL (Publish to web -> CSV)
-  SHEET_CSV_URL: "",
-  // 2) EDIT URL (ссылка на таблицу, чтобы можно было редактировать)
-  SHEET_EDIT_URL: ""
+  // PUBLISHED CSV URL (откуда карта читает данные)
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRuae5w34AoVUTQr-t1aq6keoKBO9W0CEVEj6kDSsc9aMFCiEqW04cdaZcgT94g3Zi9xElXE1mbB9Y3/pub?gid=0&single=true&output=csv",
+  
+  // EDIT URL (ссылка для преподавателя, чтобы удалённо редактировать)
+  SHEET_EDIT_URL: "https://docs.google.com/spreadsheets/d/1ROkIvACXeX_YBUjK1d2q4pm2ErDV8F7oE5SwX93hXK4/edit?usp=sharing"
 };
